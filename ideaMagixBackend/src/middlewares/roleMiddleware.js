2b7ae487fc34a4b923bd86fd596fd5238c800111
Kaustubh-Dalvi1001 <kaustubhdalvi1001@gmail.com>
2026-08-19 16:43:39 +0530

    Add role middleware (isAdmin, isInstructor)

--- ideaMagixBackend/src/middlewares/roleMiddleware.js
+++ ideaMagixBackend/src/middlewares/roleMiddleware.js
@@ -1,3 +1,13 @@
-export const roleMiddleware = (reqBody) => {
+export const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Unauthorized: admin access required." });
+  }
+  next();
+};
 
-}
+export const isInstructor = (req, res, next) => {
+  if (req.user.role !== "instructor") {
+  return  res.status(403).json({ message: "Unauthorized: instructor access required." });
+  }
+  next();
+};
